21377bd541576167279785d930d36e74ecfa69e1
Zelouchan <bad1dje@gmail.com>
2025-03-31 14:24:14 +0200

    created code for current weather conditions

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
-import './style.css';
+import "./style.css";
 
 let weatherData = [];
 let forecast = [];
 
 function createCurrentWeatherDisplay() {
-    // const locationNameDiv = document.querySelector(".location");
-    const currentWeatherDiv = document.querySelector(".currentWeather");
+    const currentLocationDiv = document.querySelector(".location");
+  const currentWeatherDiv = document.querySelector(".currentWeather");
 
-    // locationNameDiv.textContent = 
-    currentWeatherDiv.textContent = `The weather is ${weatherData.conditions}`;
-    currentWeatherDiv.textContent = `The temperature is ${weatherData.temp}`;
+
+  const currentTempDiv = document.createElement("div");
+  currentTempDiv.id = "current";  
+  currentTempDiv.textContent = `It is ${weatherData.temp} degrees celcius and the weather is ${weatherData.conditions} today.`;
+
+  const currentRainDiv = document.createElement("div");
+  currentRainDiv.id = "rain";
+  currentRainDiv.textContent = `There is a ${weatherData.precipprob}% chance that it will rain today.`;
+
+  const currentRiseDiv = document.createElement("div");
+  currentRiseDiv.id = "sunUp";
+  currentRiseDiv.textContent = `Sun up: ${weatherData.sunrise}.`;
+
+  const currentSetDiv = document.createElement("div");
+  currentSetDiv.id = "sunSet";
+  currentSetDiv.textContent = `Sun set: ${weatherData.sunset}.`;
+
+  currentWeatherDiv.appendChild(currentTempDiv);
+  currentWeatherDiv.appendChild(currentRainDiv);
+  currentWeatherDiv.appendChild(currentRiseDiv);
+  currentWeatherDiv.appendChild(currentSetDiv);
 }
 
-
 fetch(
-	'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Hertford?unitGroup=metric&elements=datetime%2Cname%2Ctemp%2Cprecip%2Cprecipprob%2Cwindspeed%2Csunrise%2Csunset%2Cconditions%2Cdescription%2Cicon&include=days%2Ccurrent%2Chours&key=TM83W5Y74VRXPFP7SHC35GFZM&contentType=json'
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Hertford?unitGroup=metric&elements=datetime%2Cname%2Ctemp%2Cprecip%2Cprecipprob%2Cwindspeed%2Csunrise%2Csunset%2Cconditions%2Cdescription%2Cicon&include=days%2Ccurrent%2Chours&key=TM83W5Y74VRXPFP7SHC35GFZM&contentType=json"
 )
-	.then((response) => response.json()) 
-	.then((data) => {
-		weatherData = data.currentConditions;
-        forecast = data.days; 
-        console.log(weatherData);
-        console.table(forecast);
-        createCurrentWeatherDisplay();
-	})
-	.catch((error) => console.error('Error:', error)); 
+  .then((response) => response.json())
+  .then((data) => {
+    weatherData = data.currentConditions;
+    forecast = data.days;
+    console.log(weatherData);
+    console.table(forecast);
+    createCurrentWeatherDisplay();
+  })
+  .catch((error) => console.error("Error:", error));
